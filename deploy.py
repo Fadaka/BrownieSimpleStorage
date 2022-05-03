84dc501b4403d1953e3225f4482ff93fc0f4f6b0
Fadaka <master_kunle14@hotmail.co.uk>
2022-05-03 16:25:19 +0100

    first commit

---
+++ deploy.py
@@ -0,0 +1,35 @@
+import json
+from solcx import compile_standard, install_solc
+
+# In the video, we forget to `install_solc`
+# from solcx import compile_standard
+
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+# We add these two lines that we forgot from the video!
+print("Installing...")
+install_solc("0.6.0")
+
+# Solidity source code
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
