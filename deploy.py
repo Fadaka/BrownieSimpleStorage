d4708e0b86286b510a481ce65c759f6c76764ba8
Fadaka <master_kunle14@hotmail.co.uk>
2022-05-04 18:34:24 +0100

    using ganache blockchain

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 import json
 from solcx import compile_standard, install_solc
+from web3 import Web3, web3
 
 # In the video, we forget to `install_solc`
 # from solcx import compile_standard
@@ -33,3 +34,14 @@ with open("compiled_code.json", "w") as file:
 bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
     "bytecode"
 ]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:7545"))
+chain_id = 1337
+my_address = "0xc898Bb4fb6d0Bd5269F5915927A513c3131D10d0"
+private_key = "0x8f4158f0b9e1aa376877a8092a93ec28768c8fc8895c545a8a76cdc77c4d284b"
+
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+nonxw = w3.eth.getTranscationCount(my_address)
